b7d831a44e3b6c57ffe748550ad6a9bd4d64f034
bominnn <144202847+bominnn@users.noreply.github.com>
2026-03-25 16:57:01 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginPage from "./LoginPage";
 import DashboardPage from "./DashboardPage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [data, setData] = useState([]);
 
-  const handleLogin = ({ id, password }) => {
-    if (id.trim() && password.trim()) {
-      setIsLoggedIn(true);
+  useEffect(() => {
+    if (!isLoggedIn) return;
+
+    const username = import.meta.env.VITE_API_USERNAME;
+    const password = import.meta.env.VITE_API_PASSWORD;
+
+    const credentials = btoa(`${username}:${password}`);
+
+    fetch(`${import.meta.env.VITE_API_URL}/api/recommendations`, {
+      headers: {
+        Authorization: `Basic ${credentials}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("데이터:", data);
+        setData(data);
+      })
+      .catch((err) => console.error("에러:", err));
+  }, [isLoggedIn]);
+
+  const handleLogin = async ({ id, password }) => {
+    try {
+      const credentials = btoa(`${id}:${password}`);
+
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/recommendations`,
+        {
+          headers: {
+            Authorization: `Basic ${credentials}`,
+          },
+        },
+      );
+
+      if (res.ok) {
+        setIsLoggedIn(true);
+      } else {
+        alert("아이디 또는 비밀번호가 틀렸습니다");
+      }
+    } catch (err) {
+      console.error(err);
+      alert("서버 오류");
     }
   };
-
   const handleLogout = () => {
     setIsLoggedIn(false);
   };
@@ -18,7 +57,7 @@ function App() {
   return (
     <>
       {isLoggedIn ? (
-        <DashboardPage onLogout={handleLogout} />
+        <DashboardPage onLogout={handleLogout} data={data} />
       ) : (
         <LoginPage onLogin={handleLogin} />
       )}
